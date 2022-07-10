c7afd8119c6937c43221f3a3453f726b3490d3ee
Audxius <audxius@gmail.com>
2022-07-10 17:18:36 +0300

    pls

--- reading_a_set_amount_of_char/reading_20_characters.cpp
+++ reading_a_set_amount_of_char/reading_20_characters.cpp
@@ -5,24 +5,24 @@ int main()
 {
     int n;
     char vardas[20];
-    //establishes the needed variables
+    // establishes the needed variables(make the size of the char variable the amount of symbols you're gonna read)
 
     ifstream fd("duomenys.txt");
     fd >> n;
-    //gets the loop count
+    // gets the loop count
     fd.ignore();
-    //makes it so the next time it reads, it starts on a new line
-    //IMPORTANT
+    // makes it so the next time it reads, it starts on a new line
+    // IMPORTANT
 
     for (int i = 0; i < n; i++)
     {
         fd.get(vardas, 21);
-        //the amount of digits it's reading must be 1 larger than what's plannned to read
+        // the amount of digits it's reading must be 1 larger than what's plannned to read
 
         cout << vardas << endl;
 
         fd.ignore();
-        //makes it so the next time it reads, it starts on a new line
-        //IMPORTANT
+        // makes it so the next time it reads, it starts on a new line
+        // IMPORTANT
     }
 }
